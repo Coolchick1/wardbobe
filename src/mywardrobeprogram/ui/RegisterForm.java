54236d2d220e4e7c9ec1ef8f4477b974208d628a
IndieCode <rluiz758@gmail.com>
2015-05-21 18:22:24 +0200

    Removed defaulter header

--- src/mywardrobeprogram/ui/RegisterForm.java
+++ src/mywardrobeprogram/ui/RegisterForm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mywardrobeprogram.ui;
 
 import java.sql.SQLException;
